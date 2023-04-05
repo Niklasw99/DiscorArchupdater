8d82ed073b9f75972b2e074f551e73efec4227a4
Niklasw99 <32064953+Niklasw99@users.noreply.github.com>
2023-04-05 15:17:52 +0200

    Update AutoVersionUpdate.sh

--- AutoVersionUpdate.sh
+++ AutoVersionUpdate.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-version=$( cat version )
+discorversion=$( cat version )
 
-getlink="https://dl.discordapp.net/apps/linux/0.0.${version}/discord-0.0.${version}.tar.gz"
+getlink="https://dl.discordapp.net/apps/linux/0.0.${discorversion}/discord-0.0.${discorversion}.tar.gz"
 status_code=$(curl -w "%{http_code}\n" -s $getlink)
 
 while  [[ "$status_code" -eq 200 ]]
 do
-getlink="https://dl.discordapp.net/apps/linux/0.0.${version}/discord-0.0.${version}.tar.gz"
+getlink="https://dl.discordapp.net/apps/linux/0.0.${discorversion}/discord-0.0.${discorversion}.tar.gz"
 status_code=$(curl -w "%{http_code}\n" -s $getlink)
 if [[ "$status_code" -ne 200 ]] ; then
-    let "version=version - 1"
-    echo $version is advaliable ; echo $version >version
-    getlink="https://dl.discordapp.net/apps/linux/0.0.${version}/discord-0.0.${version}.tar.gz"
+    let "discorversion=discorversion - 1"
+    echo $discorversion is advaliable ; echo $discorversion >discorversion
+    getlink="https://dl.discordapp.net/apps/linux/0.0.${discorversion}/discord-0.0.${discorversion}.tar.gz"
     wget $getlink
     tar -xvf "discord-0.0.$version.tar.gz"
     rm ./Discord/discord.desktop
@@ -19,7 +19,7 @@ if [[ "$status_code" -ne 200 ]] ; then
     rm -rf discord*.tar.gz*
     rm -rf Discord
 else
-    let "version=version + 1"
-    echo "testing {$version}"
+    let "discorversion=discorversion + 1"
+    echo "testing {$discorversion}"
 fi
 done
