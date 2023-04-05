9199fd2adf5dd7e0a537563db5d6d96ef01d2009
Niklasw99 <32064953+Niklasw99@users.noreply.github.com>
2023-04-05 15:14:52 +0200

    Update AutoVersionUpdate.sh
    Fixed Curl Issue.

--- AutoVersionUpdate.sh
+++ AutoVersionUpdate.sh
@@ -2,12 +2,12 @@
 version=$( cat version )
 
 getlink="https://dl.discordapp.net/apps/linux/0.0.${version}/discord-0.0.${version}.tar.gz"
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null $getlink)
+status_code=$(curl -w "%{http_code}\n" -s $getlink)
 
 while  [[ "$status_code" -eq 200 ]]
 do
 getlink="https://dl.discordapp.net/apps/linux/0.0.${version}/discord-0.0.${version}.tar.gz"
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null $getlink)
+status_code=$(curl -w "%{http_code}\n" -s $getlink)
 if [[ "$status_code" -ne 200 ]] ; then
     let "version=version - 1"
     echo $version is advaliable ; echo $version >version
